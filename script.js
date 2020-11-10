e844070678879480063eff69ed3b1e3519c4840b
Pablo Pizarro <pizarropablodev@hotmail.com>
2020-11-10 14:54:43 -0300

    v 0.0.2

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Developer", "Frontend React", "Freelancer"],
+    strings: ["Developer", "Frontend", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
